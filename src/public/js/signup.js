8f711f80fa6a0378bd7326807b42e8d52f6a3722
Mike <michael.vandenburgh@stonybrook.edu>
2019-10-15 22:04:06 -0400

    display verification page after submit adduser

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,10 +1,19 @@
 console.log("IN SIGNUP.JS");
 $('#form')
     .ajaxForm({
-        url : 'adduser', // or whatever
-        dataType : 'json',
-        success : function (response) {
-            alert("The server says: " + response);
+        url: 'adduser', // or whatever
+        dataType: 'json',
+        success: function (response) {
+            $.ajax({
+                url: "/verify",
+                type: "GET",
+                success: function (data) {
+                    console.log("The server says: " + response);
+                    $("#root").remove();
+                    console.log("displaying verify");
+                    document.getElementById("verify").innerHTML = data;
+                }
+            });
         }
     })
-;
+    ;
