88e232afa9d9527441ce77e7809d86c5f8b2f8f4
Mike <michael.vandenburgh@stonybrook.edu>
2019-10-17 11:49:08 -0400

    updated more login frontend stuff

--- src/public/js/login.js
+++ src/public/js/login.js
@@ -1,22 +1,44 @@
 console.log("IN login.js");
-$('#signInForm')
-    .ajaxForm({
-        url: '/login',
-        method: "POST",
-        // dataType: 'json',
-        data: new FormData($(this)),
-        success: function (response) {
-            $.ajax({
-                url: '/login',
-                method: 'GET',
-                dataType: 'json',
-                success: function (data) {
-                    // console.log("The server says: " + response);
-                    $("#login").remove();
-                    console.log("displaying home");
-                    document.getElementById("home").innerHTML = data;
-                }
-            });
-        }
+// $('#signInForm')
+//     .ajaxForm({
+//         url: '/login',
+//         method: "POST",
+//         // dataType: 'json',
+//         data: new FormData($(this)),
+//         success: function (response) {
+//             $.ajax({
+//                 url: '/login',
+//                 method: 'GET',
+//                 dataType: 'json',
+//                 success: function (data) {
+//                     // console.log("The server says: " + response);
+//                     $("#login").remove();
+//                     console.log("displaying home");
+//                     document.getElementById("home").innerHTML = data;
+//                 }
+//             });
+//         }
+//     });
+
+
+$('#signInForm').submit((e) => {
+    $.ajax({
+      url: $('#signInForm').attr('action'),
+      type: 'POST',
+      data : $('#signInForm').serialize(),
+      success: (response) => {
+        console.log('form submitted.');
+        console.log("the response is " + JSON.stringify(response));
+        $.ajax({
+            url: '/login',
+            type: 'GET',
+            success: (data) => {
+                $("#login").remove();
+                console.log("displaying home");
+                document.getElementById("home").innerHTML = data;
+            }
+        });
+      }
     });
-;
+    e.preventDefault();
+});
