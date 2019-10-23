ce100e57f975c803741b3a79ec825f832cd098e2
Mike <michael.vandenburgh@stonybrook.edu>
2019-10-22 22:05:34 -0400

    finished GUI for milestone 1 (for real)

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -12,6 +12,30 @@ $('#form')
                     $("#root").remove();
                     console.log("displaying verify");
                     document.getElementById("verify").innerHTML = data;
+                    console.log("#form1");
+                    $('#form1').submit((e) => {
+                        console.log(e + " inside verifyForm");
+                        e.preventDefault();
+                        $.ajax({
+                            type: 'POST',
+                            url: '/verify',
+                            data: $("#form1").serialize(),
+                            dataType: 'json',
+                            success: (response) => {
+                                console.log("SUCCESS " + response);
+                                console.log('form submitted.');
+                                console.log("the response is " + JSON.stringify(response));
+                                if (response.status === "OK") {
+                                    alert("Verification successful, please login.");
+                                    window.location.href = "/";
+                                }
+                                else {
+                                    console.log("ERROR: INCORRECT U/P");
+                                    window.location.href = "/";
+                                }
+                            }
+                        });
+                    });
                 }
             });
         }
