37b8fad3ba7ffa5000f9262299f81f7460c9c08b
Mike <michael.vandenburgh@stonybrook.edu>
2019-11-02 18:45:06 -0400

    more searching implemented

--- src/server.js
+++ src/server.js
@@ -116,7 +116,7 @@ app.post('/adduser', (req, res) => {
     let password = crypto.createHash('sha256').update(req.body.password).digest('base64');
     User.findOne({ username: username }, (err, user1) => {
         if (user1) {
-            res.json({status:"error", error:"That username is already taken."});
+            res.json({ status: "error", error: "That username is already taken." });
         }
         else {
             let key = crypto.createHash('sha256').update(username + email + "secretkey").digest('base64');
@@ -132,7 +132,7 @@ app.post('/adduser', (req, res) => {
             });
         }
     });
-    
+
 });
 
 app.post('/login', (req, res) => {
@@ -342,40 +342,42 @@ app.delete('/item/:id', (req, res) => {
 app.post('/search', (req, res) => {
     let timestamp = Number(req.body.timestamp);
     let limit = Number(req.body.limit);
-    let qe = req.body.qe;
-    let username = req.body.username;
+    let qe = req.body.q;
+    let username = String(req.body.username);
     let following = req.body.following;
     if (!timestamp) timestamp = Date.now() / 1000;
     if (!limit) limit = 25;
     if (limit > 100) limit = 100;
-    if (typeof following === undefined) following = true;
+    if (typeof following === "undefined" || following === "true") following = true;
+    else if (following === "false") following = false;
+    console.log("following: " + following);
     let items = [];
     console.log("searching for posts made before " + timestamp + " (limit:" + limit + ")...");
     let query = {
-        timestamp: {$lt: timestamp},
+        timestamp: { $lt: timestamp },
     }
-    if (qe) query.content = {$regex: qe};
-    if (username) query.username = {username};
+    if (qe) query.content = { $regex: qe };
+    if (username) query.username = username;
     let cookie = req.cookies.jwt;
     if (!cookie) cookie = "";
-    User.findOne({token:cookie}, (err, loggedInUser) => {
-        Tweet.find(query).limit(limit).then((tweets) => {
-            tweets.forEach((tweet) => {
-                User.findOne({username: tweet.username}, (err, user) => {
-                    if ((loggedInUser && user.followers.includes(loggedInUser.username)) || !loggedInUser) {
-                        items.push(tweet);
-                        console.log(tweet);
-                    }
-                });
-            });
-            console.log("RESPONSE: {status: 'OK', items: " + items + "\n}");
+    User.findOne({ token: cookie }, (err, loggedInUser) => {
+        Tweet.find(query).limit(limit).then(async (tweets) => {
+            for (tweet of tweets) {
+                let userQuery = User.findOne({ username: tweet.username });
+                let user = await userQuery.exec();
+                if ((loggedInUser && (user.followers.includes(loggedInUser.username) || !following)) || !loggedInUser) {
+                    items.push(tweet);
+                    console.log(items);
+                }
+                console.log("RESPONSE: {status: 'OK', items: " + items + "\n}");
+               
+            }
             res.json({
                 status: "OK",
                 items: items
             });
-        });
     });
-    
+});
 });
 
 app.get('/user/:username', (req, res) => {
